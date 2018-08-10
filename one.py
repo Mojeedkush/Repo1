7c197806c9ac6c545ed4e471053594cb7aca5f51
Mojeedkush <42240376+Mojeedkush@users.noreply.github.com>
2018-08-10 10:21:43 +0100

    Add files via upload

---
+++ one.py
@@ -0,0 +1,18 @@
+print ('Welcome dear user')
+print ('Kindly fill in the value of the following')
+
+a = int (input ('a='))
+b = int (input ('b='))
+c = int (input ('c='))
+x = (-b+4*a*c)/2*a
+print (x)
+
+print ('Thank you for that')
+print ('Before you go, please fill the second section')
+
+input ('Name:')
+t = int (input ('Age:'))
+s = (100-t) + 2018
+
+print ('So you will be 100 years by:',s)
+print ('Thank you for your time, do have a nice day')
